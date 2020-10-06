bd37366b86baca89765804474793a7979c20b8ca
Sarath-Molathoti <sarathmolathoti@gmail.com>
2020-10-06 09:57:43 +0530

    started

--- src/main/java/com/hackathon/Test/controller/QuestionsController.java
+++ src/main/java/com/hackathon/Test/controller/QuestionsController.java
@@ -1,14 +1,10 @@
 package com.hackathon.Test.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.hackathon.Test.service.QuestionsService;
 
 @RestController
 public class QuestionsController {
 	
-	@Autowired
-	private QuestionsService qnsService;
+	
 
 }
